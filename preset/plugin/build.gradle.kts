e3e0c0688bec87be7d38de09c4bfade9c7a9128a
Florian Buenzli <buenzli@ubique.ch>
2024-10-01 16:07:57 +0200

    maven central release

--- preset/plugin/build.gradle.kts
+++ preset/plugin/build.gradle.kts
@@ -1,64 +1,48 @@
+import com.vanniktech.maven.publish.GradlePublishPlugin
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm")
-    `java-gradle-plugin`
-    alias(libs.plugins.pluginPublish)
-    alias(libs.plugins.jfrog.artifactory)
+	kotlin("jvm")
+	`java-gradle-plugin`
+	alias(libs.plugins.pluginPublish)
+	alias(libs.plugins.vanniktech)
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation(gradleApi())
+	implementation(kotlin("stdlib"))
+	implementation(gradleApi())
 
-    implementation(libs.agp)
-    implementation(libs.kotlin.gradle)
+	implementation(libs.agp)
+	implementation(libs.kotlin.gradle)
 
-    implementation(libs.coroutines)
-    implementation(libs.okhttp)
-    implementation(libs.retrofit)
-    implementation(libs.retrofitConverterScalars)
-    implementation(libs.moshi)
-    implementation(libs.moshiKotlin)
-    implementation(libs.moshiAdapters)
-
-    testImplementation(libs.junit)
+	testImplementation(libs.junit)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<KotlinCompile> {
-    compilerOptions.jvmTarget = JvmTarget.JVM_17
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
+	compilerOptions.jvmTarget = JvmTarget.JVM_17
+	kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
 }
 
 gradlePlugin {
-    plugins {
-        create(property("ID").toString()) {
-            id = property("ID").toString()
-            implementationClass = property("IMPLEMENTATION_CLASS").toString()
-            version = project.version
-            description = property("DESCRIPTION").toString()
-            displayName = property("DISPLAY_NAME").toString()
-            tags = listOf("android", "ubique")
-        }
-    }
-    website.set(property("WEBSITE").toString())
-    vcsUrl.set(property("VCS_URL").toString())
+	plugins {
+		create(property("ID").toString()) {
+			id = property("ID").toString()
+			implementationClass = property("IMPLEMENTATION_CLASS").toString()
+			version = project.version
+		}
+	}
 }
 
-tasks.create("setupPluginUploadFromEnvironment") {
-    doLast {
-        val key = System.getenv("GRADLE_PUBLISH_KEY")
-        val secret = System.getenv("GRADLE_PUBLISH_SECRET")
-        if (key == null || secret == null) {
-            throw GradleException("GRADLE_PUBLISH_KEY and/or GRADLE_PUBLISH_SECRET are not defined environment variables")
-        }
-        System.setProperty("gradle.publish.key", key)
-        System.setProperty("gradle.publish.secret", secret)
-    }
+mavenPublishing {
+	configure(GradlePublishPlugin())
+	coordinates(property("GROUP").toString(), property("ARTIFACT_ID").toString(), project.version.toString())
+	publishToMavenCentral(SonatypeHost.S01, true)
+	signAllPublications()
 }
