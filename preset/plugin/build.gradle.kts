7563cc3ed75a50d96f54fc4805baa2ebdd51eada
Florian Buenzli <buenzli@ubique.ch>
2025-07-02 22:53:50 +0200

    publish to central portal

--- preset/plugin/build.gradle.kts
+++ preset/plugin/build.gradle.kts
@@ -1,5 +1,4 @@
 import com.vanniktech.maven.publish.GradlePublishPlugin
-import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -27,7 +26,6 @@ java {
 
 tasks.withType<KotlinCompile> {
 	compilerOptions.jvmTarget = JvmTarget.JVM_17
-	kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
 }
 
 gradlePlugin {
@@ -43,6 +41,6 @@ gradlePlugin {
 mavenPublishing {
 	configure(GradlePublishPlugin())
 	coordinates(property("GROUP").toString(), property("ARTIFACT_ID").toString(), project.version.toString())
-	publishToMavenCentral(SonatypeHost.S01, true)
+	publishToMavenCentral(true)
 	signAllPublications()
 }
